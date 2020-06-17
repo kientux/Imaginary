7b85cad040664cd9be696351016a3227b9918169
kiennt <kiennt@sapo.vn>
2020-06-17 17:21:19 +0700

    Fix SPM dependencies

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@ import PackageDescription
 let package = Package(
     name: "Imaginary",
     // platforms: [.iOS("8.0"), .macOS("10.10"), .tvOS("9.0"), .watchOS("2.0")],
-    dependencies: [
-        .package(url: "https://github.com/kientux/Cache.git", .upToNextMajor(from: "5.3.0")),
-    ],
     products: [
         .library(name: "Imaginary", targets: ["Imaginary"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kientux/Cache.git", .upToNextMajor(from: "5.3.0")),
     ],
     targets: [
         .target(
